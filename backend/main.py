7f5d2dfd834d09f0a04f809b0568b45ad0963832
cash-i1 <cashignitro@gmail.com>
2024-03-23 20:37:40 +1100

    backend now works

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,22 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
+def get_json():
+    with open("./visits.json", "r") as file:
+        return json.loads(file.read())
+        
+def write_json(json_data):
+    with open("./visits.json", "w") as file:
+        return json.dump(json_data, file)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/add_1')
+def add_1():
+    json = get_json()
+    json["visits"] += 1
+    write_json(json)
+    return json
 
 if __name__ == "__main__":
     app.run()
